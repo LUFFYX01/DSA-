c2224bb9e77e11b8b3aa008413e17ea34267374b
LUFFYX01 <prateek05092005@gmail.com>
2026-06-18 20:23:49 +0530

    new question added

---
+++ patterns/pattern2.java
@@ -0,0 +1,16 @@
+public class pattern2 {
+    public void pat(int n){
+        for (int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        pattern2 p = new pattern2();
+        p.pat(6);
+    }
+}
